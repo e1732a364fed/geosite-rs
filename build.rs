e4459ea2a240e35a6f95450d357e59cf41bba363
e1732a364fed <75717694+e1732a364fed@users.noreply.github.com>
2025-02-15 17:24:08 -0800

    minor rename

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ fn main() {
     // let out_dir = std::env::var("OUT_DIR").unwrap();
     // println!("{}", out_dir);
     // target/<profile>/build/<crate-name>-<hash>/out
-    // prost_build::compile_protos(&["src/router.proto"], &["src"]).unwrap();
+    // prost_build::compile_protos(&["src/data.proto"], &["src"]).unwrap();
 }
